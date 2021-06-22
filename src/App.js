dba650e0079ece0c8236621bb7bc2acabbcb4aa6
mohalenezi <ot.m.alenezi@gmail.com>
2021-06-22 17:18:02 +0300

    comiit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import DirectList from "./componenets/DirectList";
+import { GlobalStyle } from "./styles";
+import { useState } from "react";
+import DirectItem from "./componenets/DirectItem";
+import perfumes from "./products";
+import Home from "./componenets/Home";
+
+import { Route, Switch } from "react-router";
+import NavBar from "./componenets/NavBar";
+
+const theme = {
+  light: {
+    mainColor: "grey",
+    backgroundColor: "lightyellow",
+    red: "red",
+  },
+  dark: {
+    mainColor: "lightyellow",
+    backgroundColor: "grey",
+    red: "red",
+  },
+};
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (currentTheme === "light") setCurrentTheme("dark");
+    else setCurrentTheme("light");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme[currentTheme]}>
+        <GlobalStyle />
+        <NavBar currentTheme={currentTheme} toggleTheme={toggleTheme} />
+        <Switch>
+          <Route path="/perfumes/:perfumeSlug">
+            <DirectItem perfumes={_newperfume} perfumeDelete={perfumeDelete} />
+          </Route>
+          <Route path="/perfumes">
+            <DirectList perfumes={_newperfume} perfumeDelete={perfumeDelete} />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
